886b4358ce13837288439b36b5fbb05d1b7112c8
Frankie Liu <frankie.y.liu@gmail.com>
2023-05-21 04:26:18 +0100

    rest of setup

--- setup.py
+++ setup.py
@@ -5,5 +5,17 @@ import os
 setup(
         name='colabxterm',
         packages=['colabxterm'],
-        version="0.0.0"
+        version="0.0.0",
+        package_data={
+            'colabxterm': ['client/dist/*','VERSION']
+        },
+        include_package_data=False,
+        install_requires=['ptyprocess~=0.7.0', 'tornado>5.1'],
+        extras_require={
+            'dev': [
+                'jupyter',
+                'twine'
+            ],
+
+        }
         )
