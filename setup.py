ffdee964a0b840f449222967ea96f93b9c52f0b0
Frankie Liu <frankie.y.liu@gmail.com>
2023-05-21 01:49:34 +0100

    Add version

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from distutils.core import setup
+import os
+
+def _get_version():
+    version_file = os.path.normpath(os.path.join(os.path.dirname(__file__), 'colabxterm', 'VERSION'))
+    with open(version_file) as fh:
+        version = fh.read().strip()
+        return version
+
 setup(
         name='colabxterm',
+        version=_get_version(),
         packages=['colabxterm']
         )
